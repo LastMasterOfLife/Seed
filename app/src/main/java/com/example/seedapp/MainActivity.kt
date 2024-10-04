1ea57c144d5f2619f8c7f0ffbfef4fee635b759f
AndreaConrado <conrado.andrea@gmail.com>
2024-10-04 10:46:14 +0200

    commit corretto

--- app/src/main/java/com/example/seedapp/MainActivity.kt
+++ app/src/main/java/com/example/seedapp/MainActivity.kt
@@ -1,20 +1,58 @@
 package com.example.seedapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.seedapp.databinding.ActivityMainBinding
+import com.example.seedapp.fragments.CommunityFragment
+import com.example.seedapp.fragments.HomeFragment
+import com.example.seedapp.fragments.SeedFragment
+import com.example.seedapp.fragments.SettingsFragment
+import com.example.seedapp.fragments.UserFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        // Set default fragment
+        supportFragmentManager.beginTransaction().replace(binding.container.id, SeedFragment()).commit()
+
+        // default icon Navigationbar
+        bottomNavigationView.selectedItemId = R.id.seed
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.game -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container, HomeFragment()).commit()
+                    true
+                }
+                R.id.community -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container, CommunityFragment()).commit()
+                    true
+                }
+                R.id.seed -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container, SeedFragment()).commit()
+                    true
+                }
+                R.id.user -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container, UserFragment()).commit()
+                    true
+                }
+                R.id.settings -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container, SettingsFragment()).commit()
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
